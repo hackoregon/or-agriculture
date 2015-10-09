711856e5cd95ac2804d1443dc8886317d0b16c64
Jeff Lalic <jefflalic@gmail.com>
2015-10-08 20:40:34 -0700

    Testing API requests. It works :tada:

---
+++ app/angular/templates/testController.js
@@ -0,0 +1,10 @@
+cropApp
+  .controller('testCtrl', function($scope, $http, $log) {
+    $http({
+      method: 'GET',
+      url: 'http://api.cropcompass.org/data/subsidy_dollars?region=Oregon%20(Statewide)'
+    }).then(function successCallback(response) {
+      $scope.subsidyDollars = response;
+      console.log(response);
+    });
+  });
