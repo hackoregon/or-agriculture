0106eab235e33502e317a06c094b8c57dfbbf06f
Jeff Lalic <jefflalic@gmail.com>
2015-10-15 20:14:08 -0700

    Returned county data sorts by harvested_acres (descending).

--- app/angular/templates/testController.js
+++ app/angular/templates/testController.js
@@ -7,15 +7,29 @@ cropApp
         'Content-Type': 'text/html; charset=utf-8'
       }
     })
-    
+
     .then(function(response) {
       $scope.harvestedAcres = response;
       console.log("hello");
       console.log(response.data.data);
-
     }, function() {
       console.log("I failed");
     });
-  });
+});
 
   // http://stackoverflow.com/questions/20987604/angular-iterate-over-json
+
+cropApp
+  .filter('orderByAcres', function() {
+    return function(items, field, reverse) {
+    var filtered = [];
+    angular.forEach(items, function(item) {
+      filtered.push(item);
+    });
+    filtered.sort(function (a, b) {
+      return (a[field] > b[field] ? 1 : -1);
+    });
+    if(reverse) filtered.reverse();
+    return filtered;
+  };
+});
