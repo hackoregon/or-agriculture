65e5b8e3fdd8896ce74070811e1ef420ed7e965b
Alex Reid <microwavedboy@gmail.com>
2015-10-21 22:30:55 -0700

    Adding table format, error handling, case insensitive querying, updating docs

--- cropcompass.py
+++ cropcompass.py
@@ -1,5 +1,5 @@
 import simplejson as json
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, abort
 import psycopg2
 import logging
 
@@ -12,6 +12,10 @@ metadata_fields = ('name',
                    'source_link')
 
 table_fields = ('commodity','year','region')
+
+# Checked later, and used to adjust WHERE clause for case insensitivity
+lower_fields = ('commodity','region')
+
 
 select_table_metadata = 'SELECT {METADATA_FIELDS} FROM metadata'.format(
                                       METADATA_FIELDS=','.join(metadata_fields))
@@ -49,9 +53,29 @@ def metadata_list_all():
     return make_json_response(metadata_lookup())
 
 @app.route('/data/<table_name>')
-def data_table(table_name):
+def row_view(table_name):
     table_metadata = metadata_lookup(table_name)
+    if not table_metadata:
+        abort(404)
     row_keys = table_fields + (table_metadata['field'],)
+    table_data = fetch_data(table_name, table_metadata)
+    response = {'error':None,
+                    'rows':len(table_data),
+                    'data':dict_many(row_keys,table_data)}
+    return make_json_response(response)
+
+@app.route('/table/<table_name>')
+def table_view(table_name):
+    table_metadata = metadata_lookup(table_name)
+    if not table_metadata:
+        abort(404)
+    row_keys = table_fields + (table_metadata['field'],)
+    table_data = fetch_data(table_name, table_metadata)
+    response = crop_dictionary(row_keys, table_metadata, table_data)
+    return make_json_response(response)
+
+
+def fetch_data(table_name, table_metadata):
     check_conn()
     with app.conn.cursor() as cur:
         where_clause, where_args = parse_where()
@@ -61,13 +85,10 @@ def data_table(table_name):
                                             WHERE_CLAUSE=where_clause,
                                             LIMIT_CLAUSE=' LIMIT %s',
                                             OFFSET_CLAUSE = ' OFFSET %s ')
-        logging.debug(cur.mogrify(query,where_args + [limit, offset]))
+#        logging.warning(cur.mogrify(query,where_args + [limit, offset]))
         cur.execute(query,where_args + [limit, offset])
         table_data = cur.fetchall()
-    response = {'error':None,
-                'rows':len(table_data),
-                'data':dict_many(row_keys,table_data)}
-    return make_json_response(response)
+    return table_data
 
 def metadata_lookup(table_name=None):
     check_conn()
@@ -76,16 +97,21 @@ def metadata_lookup(table_name=None):
             query = select_table_metadata + ' WHERE table_name = %s'
             cur.execute(query, (table_name,))
             row = cur.fetchone()
-            return dict_one(metadata_fields, row)
+            if row:
+                return dict_one(metadata_fields, row)
         else:
             cur.execute(select_table_metadata)
             metadata = []
             for row in cur:
-                row_dict = dict_one(metadata_fields, row)
-                row_dict['url'] = (request.url_root.rstrip('/') +
-                       url_for('data_table', table_name=row_dict['table_name']))
-                metadata.append(row_dict)
+                if row:
+                    row_dict = dict_one(metadata_fields, row)
+                    row_dict['row_view_url'] = (request.url_root.rstrip('/') +
+                           url_for('row_view', table_name=row_dict['table_name']))
+                    row_dict['table_view_url']  = (request.url_root.rstrip('/') +
+                           url_for('table_view', table_name=row_dict['table_name']))
+                    metadata.append(row_dict)
             return metadata
+        return None
 
 def check_conn():
     try:
@@ -102,6 +128,34 @@ def make_json_response(data):
     h['Content-Type'] = 'application/json'
     return resp
 
+def crop_dictionary(row_keys, table_metadata, table_data):
+    crop_dict = {}
+    field_name = table_metadata['field']
+
+    for row in table_data:
+        row_dict = dict_one(row_keys,row)
+        if row_dict['commodity'] not in crop_dict:
+            crop_dict[row_dict['commodity']] = []
+        updated = False
+        for record in crop_dict[row_dict['commodity']]:
+            if record['year'] == row_dict['year']:
+                if row_dict['region'] in record['regions']:
+                    record['regions'][row_dict['region']] += row_dict[field_name]
+                    updated = True
+                else:
+                    record['regions'][row_dict['region']] = row_dict[field_name]
+                    updated = True
+        if not updated:
+            new_record = {'year': row_dict['year'],
+                          'unit': table_metadata['unit'],
+                          'name': table_metadata['name'],
+                          'regions': {
+                            row_dict['region'] : row_dict[field_name]
+                          }}
+            crop_dict[row_dict['commodity']].append(new_record)
+    return crop_dict
+
+
 def parse_where():
     where_clause = []
     where_args = []
@@ -110,7 +164,11 @@ def parse_where():
         if field_param:
             field_values = field_param.split(',')
             where_args.extend(field_values)
-            where_clause.append((field + ' in (%s)') %
+            if field in lower_fields:
+                where_clause.append(('lower(' + field + ') in (%s)') %
+                                    (','.join(['lower(%s)'] * len(field_values))))
+            else:
+                where_clause.append((field + ' in (%s)') %
                                 (','.join(['%s'] * len(field_values))))
     if where_clause:
         return ('WHERE ' + ' AND '.join(where_clause), where_args)
