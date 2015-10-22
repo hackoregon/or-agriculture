b55678b75e3a071247baa2befba9669c8b18c15c
Michael Bunsen <notbot@gmail.com>
2015-10-21 23:56:58 -0700

    Add farmSize controller and template with example bar chart

---
+++ app/angular/templates/farmSize.controller.js
@@ -0,0 +1,51 @@
+cropApp
+  .controller('farmSizeCtrl', function($scope, $http, $log) {
+
+    $scope.fetchData = function(params) {
+        $http({
+
+          method: 'GET',
+          url: 'http://api.cropcompass.org/data/nass_commodity_farms?year=2012&region=' + params.county.name,
+
+        })
+        .then(function(response) {
+
+          $scope.updateChart(response.data.data);
+          $log.debug(response.data.data);
+
+        }, function() {
+
+          $log.error('Failed to fetch data from api for county: ' + params.county.name);
+
+        });
+    };
+
+    $scope.updateChart = function(data) {
+        var labels = [],
+            series = [],
+            values = [];
+
+        function onlyUnique(value, index, self) {
+            return self.indexOf(value) === index;
+        }
+
+        for (var i=0; i < data.length; i++) {
+            labels.push(data[i].commodity);
+            series.push(data[i].year);
+            values.push(data[i].farms);
+        }
+
+        $scope.labels = labels.filter( onlyUnique );
+        $scope.series = series.filter ( onlyUnique );
+        $scope.data = [values.filter( onlyUnique )];
+
+        $log.debug( $scope.labels, $scope.series, $scope.data );
+    };
+
+    $scope.$on('selectionChanged', function(event, selection) {
+        $scope.selection = selection;
+        var params = selection;
+        $scope.fetchData(params);
+    });
+
+});
