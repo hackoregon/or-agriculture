edd046e7a09dc3a9219720e76865def96f5ebd1e
Michael Bunsen <notbot@gmail.com>
2015-10-25 00:19:24 -0700

    Switch to maps that are a little more aligned. Add compressed versions that hopefully will work.

--- app/angular/templates/choropleth.controller.js
+++ app/angular/templates/choropleth.controller.js
@@ -1,18 +1,18 @@
 cropApp.controller('choroplethCtrl', function($scope) {
-  $scope.currentMap = 'assets/images/placeholder-map.png';
+  $scope.currentMap = 'assets/images/maps/oregon_county_outlines-simplified-aligned.svg';
   $scope.currentMapDescription = '';
 
   $scope.maps = [
     {
-      mapURL: 'assets/images/placeholder-map.png',
-      mapDescription: 'Veggies es placeholder lorem ipsum vos postulo essum magis kohlrabi.'
+      mapURL: 'assets/images/maps/oregon_county_outlines-simplified-aligned.svg',
+      mapDescription: 'Veggies es county lines lorem ipsum vos postulo essum magis kohlrabi.'
     },
     {
-      mapURL: 'assets/images/nonirrigatedlandcapability_range.svg',
+      mapURL: 'assets/images/maps/irrigatedlandcapability_range-simplified-aligned.svg',
       mapDescription: 'Veggies es land capability lorem ipsum vos postulo essum magis kohlrabi.'
     },
     {
-      mapURL: 'assets/images/usda_statsgo_crops.svg',
+      mapURL: 'assets/images/maps/all_crops_from_CDL_2012-simplified-aligned.svg',
       mapDescription: 'Veggies es where crops can grow lorem ipsum vos postulo essum magis kohlrabi.'
     }
   ];
