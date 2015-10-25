170ee197a5a9b7cfd84ca06268b6eb4e5f976c76
Jeff Lalic <jefflalic@gmail.com>
2015-10-24 17:52:28 -0700

    Update button styles and include map description below map selectors.

--- app/angular/templates/choropleth.controller.js
+++ app/angular/templates/choropleth.controller.js
@@ -1,13 +1,24 @@
 cropApp.controller('choroplethCtrl', function($scope) {
   $scope.currentMap = 'assets/images/placeholder-map.png';
+  $scope.currentMapDescription = '';
 
   $scope.maps = [
-    {mapURL: 'assets/images/placeholder-map.png'},
-    {mapURL: 'assets/images/nonirrigatedlandcapability_range.svg'},
-    {mapURL: 'assets/images/usda_statsgo_crops.svg'}
+    {
+      mapURL: 'assets/images/placeholder-map.png',
+      mapDescription: 'Veggies es placeholder lorem ipsum vos postulo essum magis kohlrabi.'
+    },
+    {
+      mapURL: 'assets/images/nonirrigatedlandcapability_range.svg',
+      mapDescription: 'Veggies es land capability lorem ipsum vos postulo essum magis kohlrabi.'
+    },
+    {
+      mapURL: 'assets/images/usda_statsgo_crops.svg',
+      mapDescription: 'Veggies es where crops can grow lorem ipsum vos postulo essum magis kohlrabi.'
+    }
   ];
 
   $scope.selectMap = function(index) {
     $scope.currentMap = $scope.maps[index].mapURL;
+    $scope.currentMapDescription = $scope.maps[index].mapDescription;
   };
 });
