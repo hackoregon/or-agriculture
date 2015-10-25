841b79fb5b6aab44921bee4dd00d949bce61b37e
Michael Bunsen <notbot@gmail.com>
2015-10-25 15:13:59 -0700

    Working number_of_farms and standardized how we fetch data to a DataFrame

--- api/charts.py
+++ api/charts.py
@@ -7,13 +7,28 @@ import pandas
 
 
 
-def _get_data(table_name, url_args={}):
+def _get_data(table_name, url_args={}, as_dataframe=True):
     table_metadata = db.metadata_lookup(table_name)
     if not table_metadata:
         abort(404)
     row_keys = db.table_fields + (table_metadata['field'],)
     table_data = db.fetch_data(table_name, table_metadata, url_args)
-    return row_keys, table_data
+
+    if as_dataframe:
+        columns, data = row_keys, table_data
+
+        if not len(data):
+            abort(404)
+        df = pandas.DataFrame.from_records(data, columns=columns)
+
+        # Fill NA values with a regular "None":
+        # which we have to do before serializing to JSON
+        df = df.where((pandas.notnull(df)), None)
+
+        return df
+
+    else:
+        return row_keys, table_data
 
 
 def test():
@@ -27,14 +42,7 @@ def test():
 def pandas_test():
     table_name = 'nass_commodity_area'
     
-    columns, data = _get_data(table_name)
-
-    df = pandas.DataFrame.from_records(data, columns=columns)
-
-    # Fill nulls with zero:
-    # df.fillna(0)
-    # Or fill with "None":
-    df = df.where((pandas.notnull(df)), None)
+    df = _get_data(table_name)
 
     return df.to_dict('records')
 
@@ -63,7 +71,7 @@ def county_rankings():
     return data
     
 
-def farm_size():
+def number_of_farms():
     """
     JSON for farm size pie chart. Should look like this:
     {'total_farms': 300', 'farms_for_commodity': 25}
@@ -73,37 +81,61 @@ def farm_size():
 
     table_name = 'nass_commodity_farms'
     
-    args = {'year': 2012,
-            'region': 'Yamhill',
-            'commodity': 'Walnuts'}
-    columns, data = _get_data(table_name, args)
-    df = pandas.DataFrame.from_records(data, columns=columns)
+    #args = {'year': 2007,
+    #        'region': 'Yamhill',}
 
-    # Fill nulls with zero:
-    # df.fillna(0)
-    # Or fill with "None":
-    df = df.where((pandas.notnull(df)), None)
+    df = _get_data(table_name)
 
     # /data/nass_commodity_farms?region=Yamhill&limit=5000&commodity=Total Farms&year=2012
     # /data/nass_commodity_farms?region=Yamhill&limit=5000&commodity=Hazelnuts&year=2012
 
     # We don't have access to the original URL query params here,
     # so we grab the first commodity & region here
-
     selected_year = df.year.unique()[0]
     selected_commodity = df.commodity.unique()[0]
     selected_region = df.region.unique()[0]
 
+    # Make sure we filter down to selection
     df = df[df.year == selected_year]
     df = df[df.region == selected_region]
-    total_farms = df.pivot_table(index=['year', 'region', 'commodity'], values='farms', aggfunc=sum)
-
     df = df[df.commodity == selected_commodity]
-    farms_for_commodity = df.pivot_table(index='region', values='farms', aggfunc=sum)
+
+    farms_per_commodity = df.pivot_table(
+            index='region',
+            values='farms',
+            aggfunc=sum).values[0]
+
+
+    # Do a 2nd query for Total Farms because if they
+    # filtered by a specific commodity it gets excluded
+    selected_commodity = 'Total Farms'
+    args = {'region': selected_region,
+            'commodity': selected_commodity}
+    df = _get_data(table_name, args)
+
+    # Make sure we filter down to selection
+    df = df[df.year == selected_year]
+    df = df[df.region == selected_region]
+    df = df[df.commodity == selected_commodity]
+
+    total_farms = df.pivot_table(
+            index='region',
+            values='farms',
+            aggfunc=sum).values[0]
+
+
+    json = [ {
+        "region": selected_region,
+        "commodity": selected_commodity,
+        "year": selected_year,
+        "farms_per_commodity": farms_per_commodity,
+        "total_farms": total_farms,
+    }]
 
     #import ipdb; ipdb.set_trace()
     #return df.to_dict('records')
-    return df.to_dict('records') 
+    #return totals.to_dict('records') 
+    return json
 
 
 
@@ -118,17 +150,14 @@ def crop_diversity():
     default_year = 2012
     default_region = 'Yamhill'
     
-    columns, data = _get_data(table_name)
+    df = _get_data(table_name)
 
     diversity = {}
-    df = pandas.DataFrame.from_records(data, columns=columns)
 
     regions = df.region.unique()
     crops_and_acres = df[['commodity', 'acres']][df.year == default_year][df.region == default_region]
     croparea = crops_and_acres.groupby('commodity').sum()
 
-    # Seems like the only way to fillna() with a regular "None"
-    # which we have to do before serializing to JSON
     croparea = croparea.where((pandas.notnull(df)), None)
 
     data = croparea.to_dict('records')
