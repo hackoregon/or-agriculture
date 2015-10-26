00704da88bdfbfc6e7b5c813e8486b94a87d73ef
Jeff Lalic <jefflalic@gmail.com>
2015-10-26 15:21:27 -0700

    Shorten map description for land capability. Quick fix to prevent pushing down content to next section.

--- app/angular/templates/choropleth.controller.js
+++ app/angular/templates/choropleth.controller.js
@@ -2,7 +2,7 @@ cropApp.controller('choroplethCtrl', function($scope) {
   $scope.mapInfo = [
     {
       mapName: "Land Capability",
-      mapDescription: "Land Capability Classification is a measure of the suitability of soils for field crops. The classification system was developed by the US Department of Agriculture to inform farm planning and soil manament. Soil is divided into eight capability classes, ranging from Class 1 (soils have slight limitations that restrict agricultural use) to Class 8 (soils or area are unsuitable for agriculture). With proper management, soils in classes 1-4 can be utilized to grow crops with long-term productivity, while soils in classes 4-8 are not suitable for commercial crop production and are best used for pasture, forestland, or non-agricultural uses.  Soils are classified for both irrigated and non-irrigated agriculture; the 'Land Capability' map on crop compass shows the soil classification for Oregon land suitable for growing commercial crops with irrigation. See [UDEL] for more information about each class."
+      mapDescription: "Land Capability Classification is a measure of the suitability of soils for field crops (developed by USDA). Soil is divided into eight capability classes, ranging from Class 1 (soils have slight limitations that restrict agricultural use) to Class 8 (soils or area are unsuitable for agriculture). With proper management, soils in classes 1-4 can be utilized to grow crops with long-term productivity, while soils in classes 4-8 are not suitable for commercial crop production and are best used for pasture, forestland, or non-agricultural uses."
     },
     {
       mapName: "Where crops are growing",
