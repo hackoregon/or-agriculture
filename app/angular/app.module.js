368d4dab1bcf7148614b46e0356621d32b156378
Michael Bunsen <notbot@gmail.com>
2015-10-27 12:32:32 -0700

    Fix chart scale to always start at zero

--- app/angular/app.module.js
+++ app/angular/app.module.js
@@ -1,1 +1,12 @@
-var cropApp = angular.module('cropApp', ['ngAnimate', 'ui.bootstrap', 'ui.router', 'chart.js']);
+var cropApp = angular.module('cropApp', ['ngAnimate', 'ui.bootstrap', 'ui.router', 'chart.js'])
+.config(['ChartJsProvider', function (ChartJsProvider) {
+    // Configure all charts
+    ChartJsProvider.setOptions({
+      //colours: ['#FF5252', '#FF8A80'],
+      scaleBeginAtZero: true,
+      scaleFontSize: 16
+    });
+    // Configure all line charts
+    ChartJsProvider.setOptions('Line', {
+    });
+  }]);
